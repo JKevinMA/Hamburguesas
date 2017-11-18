67503223ca6abcad54f76c859d41cd643f126c5c
kevin <kevinmamanialanya@gmail.com>
2017-11-17 22:48:37 -0500

    Pantalla Login y Main Activity(semiterminado)

--- app/src/main/java/com/example/kevin/hamburguesas/Login.java
+++ app/src/main/java/com/example/kevin/hamburguesas/Login.java
@@ -1,5 +1,6 @@
 package com.example.kevin.hamburguesas;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +12,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ActionBar AB = getSupportActionBar();
-        AB.hide();
         TextView title = (TextView)findViewById(R.id.title) ;
         title.setTypeface(Fuentes.miFuente2(this));
-
+        Intent it = new Intent(this,MainActivity.class);
+        startActivity(it);
     }
 }
